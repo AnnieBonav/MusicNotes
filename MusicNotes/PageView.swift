7611b201f1106f10452efe68eb84fb3296f98307
Annie Bonav <anniebonav@outlook.com>
2024-08-19 20:31:54 -0400

    Adds initial files

---
+++ MusicNotes/PageView.swift
@@ -0,0 +1,28 @@
+//
+//  PageView.swift
+//  MusicNotes
+//
+//  Created by Ana Bonavides Aguilar on 8/19/24.
+//
+
+import SwiftUI
+import SwiftData
+
+
+struct PageView: View {
+    var backgroundColor = Color(.appBackground)
+    
+    var body: some View {
+        VStack {
+            Text("Page Title")
+                .font(.title).padding(.top)
+        }
+        .padding(.leading).background(backgroundColor)
+        .frame(maxWidth: .infinity, alignment: .leading)
+        Spacer()
+    }
+}
+
+#Preview {
+    PageView()
+}
