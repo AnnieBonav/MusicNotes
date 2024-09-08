2c15ec4bfc413439c7b09b25dd13716e1721c497
Annie Bonav <anniebonav@outlook.com>
2024-09-07 19:38:55 -0700

    Adds files before stash

---
+++ Thoughtful/DataModels/PhotoData.swift
@@ -0,0 +1,29 @@
+import SwiftData
+import Foundation
+
+@Model
+final class PhotoData{
+    var fileName: String?
+    var location = Location.documents
+    
+    init(
+        fileName: String? = nil) {
+        self.fileName = fileName
+    }
+    
+    var url: URL? {
+        if location == .resources {
+            if let jpegImage = Bundle.main.url(forResource: fileName, withExtension: "jpeg") {
+                return jpegImage
+            } else {
+                return Bundle.main.url(forResource: fileName, withExtension: "png")
+            }
+        } else {
+            guard let fileName else {
+                return nil
+            }
+            let documentDirectory = FileManager.default.documentDirectory
+            return documentDirectory.appendingPathComponent(fileName)
+        }
+    }
+}
